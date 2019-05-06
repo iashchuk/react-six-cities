35632beac83b03f3865bf4bcda7a018e4571d6e4
iashchuk <221box@list.ru>
2019-05-06 23:29:36 +0300

    Добавляет снепшот-тест для Card

---
+++ src/components/card/card.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Card from "./card.jsx";
+import renderer from "react-test-renderer";
+
+const card = {
+  title: `Beautiful & luxurious apartment at great location`,
+  image: `apartment-01.jpg`,
+  type: `Apartment`,
+  price: 120,
+  rating: 93,
+  isPremium: true
+};
+
+describe(`Card`, () => {
+  it(`renders correctly`, () => {
+    const component = renderer
+      .create(
+          <Card
+            title={card.title}
+            image={card.image}
+            type={card.type}
+            price={card.price}
+            rating={card.rating}
+            isPremium={card.isPremium}
+            onCardTitleClick={jest.fn()}
+          />
+      )
+      .toJSON();
+
+    expect(component).toMatchSnapshot();
+  });
+});
