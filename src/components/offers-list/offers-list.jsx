a40b914bce994914b1dd2959afa30299aa4ead23
iashchuk <221box@list.ru>
2019-05-12 14:44:26 +0300

    Добавляет компонент OffersList

---
+++ src/components/offers-list/offers-list.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Card from "../card/card.jsx";
+
+const OffersList = ({cards, onCardTitleClick}) => {
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      {cards.map((item, index) => {
+        return (
+          <Card
+            key={index}
+            title={item.title}
+            image={item.image}
+            type={item.type}
+            price={item.price}
+            rating={item.rating}
+            isPremium={item.isPremium}
+            onCardTitleClick={onCardTitleClick}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+OffersList.propTypes = {
+  cards: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        rating: PropTypes.number,
+        isPremium: PropTypes.bool
+      })
+  ),
+  onCardTitleClick: PropTypes.func
+};
+
+export default OffersList;
