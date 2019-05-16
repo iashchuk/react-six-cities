1bb6a03dc44f4ab9852e362aa7805eca95f39a70
iashchuk <221box@list.ru>
2019-05-16 23:27:31 +0300

    Исправляет замечания

--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -2,7 +2,28 @@ import React from "react";
 import Main from "./main.jsx";
 import renderer from "react-test-renderer";
 
-import offersData from "../../mocks/offers";
+const offersData = [
+  {
+    title: `Beautiful & luxurious apartment at great location`,
+    image: `apartment-01`,
+    imageExtension: `jpg`,
+    type: `Apartment`,
+    price: 120,
+    currency: `€`,
+    rating: 93,
+    isPremium: true
+  },
+  {
+    title: `Wood and stone place`,
+    image: `room`,
+    imageExtension: `jpg`,
+    type: `Private room`,
+    price: 80,
+    currency: `€`,
+    rating: 80,
+    isPremium: false
+  }
+];
 
 describe(`Main`, () => {
   it(`renders correctly`, () => {
