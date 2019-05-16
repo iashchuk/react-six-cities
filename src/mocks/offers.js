d69c5bf266748af1a4767950ffd7fef3754e8a78
iashchuk <221box@list.ru>
2019-05-16 23:31:27 +0300

    Обновляет моковые данные

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -7,7 +7,8 @@ export default [
     price: 120,
     currency: `€`,
     rating: 93,
-    isPremium: true
+    isPremium: true,
+    coords: [52.3909553943508, 4.85309666406198]
   },
   {
     title: `Wood and stone place`,
@@ -17,7 +18,8 @@ export default [
     price: 80,
     currency: `€`,
     rating: 80,
-    isPremium: false
+    isPremium: false,
+    coords: [52.369553943508, 4.85309666406198]
   },
   {
     title: `Canal View Prinsengracht`,
@@ -27,7 +29,8 @@ export default [
     price: 132,
     currency: `€`,
     rating: 80,
-    isPremium: false
+    isPremium: false,
+    coords: [52.3909553943508, 4.929309666406198]
   },
   {
     title: `Nice, cozy, warm big bed apartment`,
@@ -37,7 +40,8 @@ export default [
     price: 180,
     currency: `€`,
     rating: 100,
-    isPremium: true
+    isPremium: true,
+    coords: [52.3809553943508, 4.939309666406198]
   },
   {
     title: `Wood and stone place`,
