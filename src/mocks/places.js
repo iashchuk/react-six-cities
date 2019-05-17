d38e5b8087eb175b0c2a7e3882dc8e454288cd43
iashchuk <221box@list.ru>
2019-05-17 22:07:03 +0300

    Выделяет список городов

---
+++ src/mocks/places.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    city: `Paris`,
+    active: false
+  },
+  {
+    city: `Cologne`,
+    active: false
+  },
+  {
+    city: `Brussels`,
+    active: false
+  },
+  {
+    city: `Amsterdam`,
+    active: true
+  },
+  {
+    city: `Hamburg`,
+    active: false
+  },
+  {
+    city: `Dusseldorf`,
+    active: false
+  }
+];
