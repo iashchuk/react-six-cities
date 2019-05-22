6462d330036c0371b7f4942655f3c21cf00b2b0a
iashchuk <221box@list.ru>
2019-05-22 22:56:30 +0300

    Дорабатывает тест для компонента App

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,11 +2,19 @@ import React from "react";
 import App from "./app.jsx";
 import renderer from "react-test-renderer";
 import Map from "../map/map.jsx";
+import { Provider } from "react-redux";
+import { store } from "../../store/index.js";
 
 describe(`App`, () => {
   it(`renders correctly`, () => {
     Map.prototype.componentDidMount = jest.fn();
-    const component = renderer.create(<App />).toJSON();
+    const component = renderer
+      .create(
+          <Provider store={store}>
+            <App />
+          </Provider>
+      )
+      .toJSON();
 
     expect(component).toMatchSnapshot();
   });
