afa1b52c0c3ab483c435f68ff3b05264d870e26a
iashchuk <221box@list.ru>
2019-05-26 22:21:59 +0300

    Устанавливает и настраивает redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,21 @@
-import { createStore, compose } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { createLogger } from "redux-logger";
 import { rootReducer } from "./rootReducer";
+
+const logger = createLogger({
+  duration: true,
+  collapsed: true,
+  colors: {
+    title: () => `#139BFE`,
+    prevState: () => ` #1C5FAF`,
+    action: () => `#149945`,
+    nextState: () => `#A47104`,
+    error: () => `#FF0005`
+  }
+});
 
 const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 const composeEnhancers = devtools ? devtools : compose;
-const enhansedStore = composeEnhancers();
+const enhansedStore = composeEnhancers(applyMiddleware(logger));
 
 export const store = createStore(rootReducer, enhansedStore);
