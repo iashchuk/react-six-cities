173e5f87439688b2550afc5aca4a8002f77098c8
iashchuk <221box@list.ru>
2019-05-29 23:53:35 +0300

    Добавляет E2E-тест для withActiveItem

---
+++ src/hocs/with-active-item.e2e.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import withActiveItem from "./with-active-item";
+
+configure({ adapter: new Adapter() });
+
+const Component = () => <div>This is a component!</div>;
+const WithActiveItem = withActiveItem(Component);
+
+it(`should set activeItem`, () => {
+  const component = shallow(<WithActiveItem />);
+  expect(component.props().activeItem).toEqual(``);
+  component.props().setActiveItem(`Paris`);
+  expect(component.props().activeItem).toEqual(`Paris`);
+});
