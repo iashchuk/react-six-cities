da3560639c0e7c2ec69b7472ddde4b19bd3227dc
iashchuk <221box@list.ru>
2019-06-03 00:32:38 +0300

    Обновляет тесты

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,20 +1,21 @@
 import React from "react";
 import App from "./app.jsx";
-import renderer from "react-test-renderer";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import Map from "../map/map.jsx";
 import { Provider } from "react-redux";
 import { store } from "../../redux/index.js";
 
+configure({ adapter: new Adapter() });
+
 describe(`App`, () => {
   it(`renders correctly`, () => {
     Map.prototype.componentDidMount = jest.fn();
-    const component = renderer
-      .create(
-          <Provider store={store}>
-            <App />
-          </Provider>
-      )
-      .toJSON();
+    const component = shallow(
+        <Provider store={store}>
+          <App />
+        </Provider>
+    );
 
     expect(component).toMatchSnapshot();
   });
--- src/components/offers-list/offers-list.test.js
+++ src/components/offers-list/offers-list.test.js
@@ -2,34 +2,36 @@ import React from "react";
 import renderer from "react-test-renderer";
 import OffersList from "./offers-list.jsx";
 
+const city = `Amsterdam`;
+
 const cards = [
   {
     title: `Beautiful & luxurious apartment at great location`,
-    image: `apartment-01`,
-    imageExtension: `jpg`,
+    image: `apartment-01.jpg`,
     type: `Apartment`,
     price: 120,
     currency: `€`,
     rating: 93,
     isPremium: true,
-    coords: [52.3909553943508, 4.85309666406198]
+    location: { latitude: 52.3909553943508, longitude: 4.85309666406198 }
   },
   {
     title: `Wood and stone place`,
-    image: `room`,
-    imageExtension: `jpg`,
+    image: `room.jpg`,
     type: `Private room`,
     price: 80,
     currency: `€`,
     rating: 80,
     isPremium: false,
-    coords: [52.369553943508, 4.85309666406198]
+    location: { latitude: 52.3909553943508, longitude: 4.85309666406198 }
   }
 ];
 
 describe(`OffersList`, () => {
   it(`renders correctly`, () => {
-    const component = renderer.create(<OffersList cards={cards} />).toJSON();
+    const component = renderer
+      .create(<OffersList cards={cards} city={city} />)
+      .toJSON();
 
     expect(component).toMatchSnapshot();
   });
