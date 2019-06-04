3e2167e7474ca7db960e91f2eb685cc1991412e0
iashchuk <221box@list.ru>
2019-06-05 00:08:39 +0300

    Реализует логику авторизации

--- src/redux/auth/reducer.js
+++ src/redux/auth/reducer.js
@@ -1,10 +1,15 @@
 import * as types from "./types";
 
 const initialState = {
-  isAuthorizationRequired: true
+  isAuthorizationRequired: true,
+  id: null,
+  email: null,
+  name: null,
+  avatarUrl: null,
+  isPro: null
 };
 
-export const authReducer = (state = initialState, { type }) => {
+export const authReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case types.SET_AUTH_REQUIRED:
       return {
@@ -16,6 +21,16 @@ export const authReducer = (state = initialState, { type }) => {
       return {
         ...state,
         isAuthorizationRequired: false
+      };
+
+    case types.LOGIN:
+      return {
+        ...state,
+        id: payload.id,
+        email: payload.email,
+        name: payload.name,
+        avatarUrl: payload.avatarUrl,
+        isPro: payload.isPro
       };
 
     default:
