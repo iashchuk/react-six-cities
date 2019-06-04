c4b61f0c964120266263104c6a390a422eafc707
iashchuk <221box@list.ru>
2019-06-04 23:59:15 +0300

    Добавляет HOC - withLogin

---
+++ src/hocs/with-login/with-login.js
@@ -0,0 +1,55 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+
+const withLogin = (Component) => {
+  class WithLogin extends PureComponent {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        email: ``,
+        password: ``
+      };
+
+      this._handleFormSubmit = this._handleFormSubmit.bind(this);
+      this._handleInputChange = this._handleInputChange.bind(this);
+    }
+
+    _handleInputChange(evt) {
+      const { value, name } = evt.target;
+
+      this.setState({
+        [name]: value
+      });
+    }
+
+    _handleFormSubmit(evt) {
+      evt.preventDefault();
+
+      const { email, password } = this.state;
+      const { loginAsync, removeAuthRequired } = this.props;
+
+      loginAsync(email, password);
+      removeAuthRequired();
+    }
+
+    render() {
+      return (
+        <Component
+          {...this.props}
+          handleInputChange={this._handleInputChange}
+          handleFormSubmit={this._handleFormSubmit}
+        />
+      );
+    }
+  }
+
+  WithLogin.propTypes = {
+    loginAsync: PropTypes.func.isRequired,
+    removeAuthRequired: PropTypes.func.isRequired
+  };
+
+  return WithLogin;
+};
+
+export default withLogin;
