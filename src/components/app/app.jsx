8f2116b4640cba890dfe71b7203d912fcfb1a195
iashchuk <221box@list.ru>
2019-06-07 00:54:50 +0300

    Добавляет роутинг в приложение

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import MainContainer from "../../containers/main-container/main-container.jsx";
 import SignInContainer from "../../containers/sign-in-container/sign-in-container.jsx";
 
 class App extends Component {
   render() {
     return (
-      <>
-        <SignInContainer />
-        <MainContainer />
-      </>
+      <Switch>
+        <Route path="/" component={MainContainer} exact />
+        <Route path="/login" component={SignInContainer} />
+      </Switch>
     );
   }
 }
