660b73716ba83b16fd42251a9fb8e4400c5254c4
iashchuk <221box@list.ru>
2019-06-21 00:07:05 +0300

    Реализует сортировку комментариев

--- src/components/reviews-list/reviews-list.jsx
+++ src/components/reviews-list/reviews-list.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import Review from "../review/review.jsx";
 
+import { sortByDate } from "../../helpers/sort-by-date";
+
 const ReviewsList = ({ comments }) => {
+  const commentToDisplay = comments.sort(sortByDate).slice(0, 10);
+
   return (
     <section className="property__reviews reviews">
       <h2 className="reviews__title">
-        Reviews &middot; <span className="reviews__amount">1</span>
+        Reviews &middot;
+        <span className="reviews__amount">{commentToDisplay.length}</span>
       </h2>
       <ul className="reviews__list">
-        {comments.map((item) => {
+        {commentToDisplay.map((item) => {
           return (
             <Review
               key={item.id}
